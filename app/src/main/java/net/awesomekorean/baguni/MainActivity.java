e36674ba89dcf6ebec55d39706cec42207ba6998
kammaii <kammaii@naver.com>
2019-06-16 15:45:09 +0700

    changed to navigation drawer

--- app/src/main/java/net/awesomekorean/baguni/MainActivity.java
+++ app/src/main/java/net/awesomekorean/baguni/MainActivity.java
@@ -1,80 +1,71 @@
 package net.awesomekorean.baguni;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    Button bt1, bt2, bt3, bt4;
-    FragmentManager fm;
-    FragmentTransaction ft;
-    Word word = new Word();
-    Lesson lesson = new Lesson();
-    Reading reading = new Reading();
-    Writing writing = new Writing();
+//    FragmentManager fm;
+//    FragmentTransaction ft;
+//    Word word = new Word();
+//    Lesson lesson = new Lesson();
+//    Reading reading = new Reading();
+//    Writing writing = new Writing();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bt1 = (Button) findViewById(R.id.button1);
-        bt2 = (Button) findViewById(R.id.button2);
-        bt3 = (Button) findViewById(R.id.button3);
-        bt4 = (Button) findViewById(R.id.button4);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        bt1.setOnClickListener(this);
-        bt2.setOnClickListener(this);
-        bt3.setOnClickListener(this);
-        bt4.setOnClickListener(this);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
 
-        setFragment(0);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button1:
-                setFragment(0);
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.menu_vocabulary:
+                Toast.makeText(getApplicationContext(),"vocabulary clicked", Toast.LENGTH_LONG).show();
                 break;
-            case R.id.button2:
-                setFragment(1);
+            case R.id.menu_lesson:
+                Toast.makeText(getApplicationContext(),"lesson clicked", Toast.LENGTH_LONG).show();
                 break;
-            case R.id.button3:
-                setFragment(2);
+            case R.id.menu_reading:
+                Toast.makeText(getApplicationContext(),"reading clicked", Toast.LENGTH_LONG).show();
                 break;
-            case R.id.button4:
-                setFragment(3);
+            case R.id.menu_writing:
+                Toast.makeText(getApplicationContext(),"writing clicked", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_profile:
+                Toast.makeText(getApplicationContext(),"profile clicked", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_option:
+                Toast.makeText(getApplicationContext(),"option clicked", Toast.LENGTH_LONG).show();
                 break;
         }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
-    private void setFragment(int i) {
-        fm = getSupportFragmentManager();
-        ft = fm.beginTransaction();
-
-        switch (i) {
-            case 0:
-                ft.replace(R.id.main_frame, word);
-                ft.commit();
-                break;
-            case 1:
-                ft.replace(R.id.main_frame, lesson);
-                ft.commit();
-                break;
-            case 2:
-                ft.replace(R.id.main_frame, reading);
-                ft.commit();
-                break;
-            case 3:
-                ft.replace(R.id.main_frame, writing);
-                ft.commit();
-                break;
-        }
-    }
 }
