c883533f5656711fd84c534f36adbbd246964f55
kammaii <kammaii@naver.com>
2019-06-17 17:22:44 +0700

    made Hangul lesson page

--- app/src/main/java/net/awesomekorean/baguni/MainHangulLesson.java
+++ app/src/main/java/net/awesomekorean/baguni/MainHangulLesson.java
@@ -1,13 +1,22 @@
 package net.awesomekorean.baguni;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainHangulLesson extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_hangul_lesson);
+    }
+
+    public void onButtonClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), HangulLesson.class);
+        startActivity(intent);
     }
 }
