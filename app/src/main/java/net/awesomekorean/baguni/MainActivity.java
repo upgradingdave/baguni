40c0535ebab42ad51fe9ff9f779ba3f45c8273b6
kammaii <kammaii@naver.com>
2019-08-04 20:56:08 +0700

    save

--- app/src/main/java/net/awesomekorean/baguni/MainActivity.java
+++ app/src/main/java/net/awesomekorean/baguni/MainActivity.java
@@ -1,35 +1,39 @@
 package net.awesomekorean.baguni;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import net.awesomekorean.baguni.lesson.LessonFrame;
 
 public class MainActivity extends AppCompatActivity implements Button.OnClickListener {
 
     FragmentManager fm;
     FragmentTransaction ft;
+
     MainLesson mainLesson = new MainLesson();
     MainReading mainReading = new MainReading();
     MainWriting mainWriting = new MainWriting();
+
+    Button btnReading;
+    Button btnWriting;
+    Button btnCollection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLesson = findViewById(R.id.btnLesson);
+        Button btnReading = findViewById(R.id.btnReading);
+        Button btnWriting = findViewById(R.id.btnWriting);
+        Button btnCollection = findViewById(R.id.btnCollection);
+        btnLesson.setOnClickListener(this);
+        btnReading.setOnClickListener(this);
+        btnWriting.setOnClickListener(this);
+        btnCollection.setOnClickListener(this);
 
         setFrag(mainLesson);
 
@@ -60,11 +64,11 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
                 break;
 
             case R.id.btnReading:
-                setFrag(mainLesson);
+                setFrag(mainReading);
                 break;
 
             case R.id.btnWriting:
-                setFrag(mainLesson);
+                setFrag(mainWriting);
                 break;
 
             case R.id.btnCollection:
