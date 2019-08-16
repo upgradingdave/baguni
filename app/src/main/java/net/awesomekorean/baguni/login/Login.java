90d471c85ba979987fe234612d524c036613c971
kammaii <kammaii@naver.com>
2019-08-17 00:02:39 +0700

    made login page

---
+++ app/src/main/java/net/awesomekorean/baguni/login/Login.java
@@ -0,0 +1,97 @@
+package net.awesomekorean.baguni.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import net.awesomekorean.baguni.R;
+
+public class Login extends AppCompatActivity implements Button.OnClickListener {
+
+    Button btnSignIn;
+    Button btnSignUp;
+    Button btnSignInGoogle;
+    Button btnSignInFacebook;
+    Button btnSelectLanguage;
+
+    ListView listView;
+    LoginListViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSingUp);
+        btnSignInGoogle = findViewById(R.id.btnSignInGoogle);
+        btnSignInFacebook = findViewById(R.id.btnSignInFacebook);
+        btnSelectLanguage = findViewById(R.id.btnSelectLanguage);
+        listView = findViewById(R.id.listView);
+        btnSignIn.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+        btnSignInGoogle.setOnClickListener(this);
+        btnSignInFacebook.setOnClickListener(this);
+        btnSelectLanguage.setOnClickListener(this);
+
+        adapter = new LoginListViewAdapter();
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String language = (String) adapterView.getItemAtPosition(i);
+                btnSelectLanguage.setText(language);
+                listView.setVisibility(View.GONE);
+
+                switch (i) {    // 선택한 언어에 따라 메뉴 언어 변경
+
+                    case 0 :
+                        break;
+
+                    case 1 :
+                        break;
+
+                    case 2 :
+                        break;
+
+                    case 3 :
+                        break;
+
+                    case 4 :
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.btnSignIn :
+                break;
+
+            case R.id.btnSingUp :
+                break;
+
+            case R.id.btnSignInGoogle :
+                break;
+
+            case R.id.btnSignInFacebook :
+                break;
+
+            case R.id.btnSelectLanguage :
+                listView.setVisibility(View.VISIBLE);
+                break;
+        }
+
+    }
+}
---
+++ app/src/main/java/net/awesomekorean/baguni/login/LoginListViewAdapter.java
@@ -0,0 +1,46 @@
+package net.awesomekorean.baguni.login;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import net.awesomekorean.baguni.R;
+
+public class LoginListViewAdapter extends BaseAdapter {
+
+    private String[] items = {"Korean", "English", "Chinese", "Japanese", "Thai"};
+
+    @Override
+    public int getCount() {
+        return items.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return items[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        final int position = i;
+        final Context context = viewGroup.getContext();
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.activity_login_listview_item, viewGroup, false);
+
+        TextView language = view.findViewById(R.id.textLanguage);
+
+        language.setText(items[i]);
+
+        return view;
+    }
+}
