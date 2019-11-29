9490192079a9dee969f13034b224a0ef578e8af1
kammaii <kammaii@naver.com>
2019-11-29 11:44:17 +0700

    applied lesson quiz3 design

--- app/src/main/java/net/awesomekorean/podo/lesson/LessonWordQuiz3.java
+++ app/src/main/java/net/awesomekorean/podo/lesson/LessonWordQuiz3.java
@@ -1,5 +1,6 @@
 package net.awesomekorean.podo.lesson;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -12,7 +13,10 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.flexbox.FlexboxLayout;
 
 import net.awesomekorean.podo.DpToPx;
 import net.awesomekorean.podo.R;
@@ -21,16 +25,29 @@ import java.util.Random;
 public class LessonWordQuiz3 extends Fragment implements Button.OnClickListener{
 
     View view;
-    static String[] wordsForQuiz; // 레슨의 단어 묶음을 퀴즈용으로 사용하기 위해 복사함
+    static String[] words; // 단어 front array
+
     String word; // 퀴즈용 단어 묶음에 있는 각 단어
+
     String[] syllables; // wordInIndex의 단어를 각 음절로 나눔
-    TextView wordQuiz1Answer; // 입력한 정답이 표시되는 텍스트뷰
-    LinearLayout wordQuiz1Selector; // 정답을 입력하는 버튼이 들어가는 layout
+
+    LinearLayout answerLayout;
+
+    TextView tvAnswer; // 입력한 정답이 표시되는 텍스트뷰
+
+    FlexboxLayout flexboxLayout; // 정답을 입력하는 버튼이 들어가는 layout
+
     Button btnSelector; // 정답을 입력하기 위해 만들어진 한글 버튼
-    Button btnReset;
+
+    ImageView btnAudio;
+    ImageView btnReset;
+
     int quizCount = 0; // 단어퀴즈 순서
+
+    MediaPlayer mediaPlayer;
+
     View.OnClickListener selectorButtonClick; // 정답 입력 버튼 클릭 시 작동하는 함수
-    ImageView correctImage; // 정답 시 나오는 이미지
+
 
     // fragment 간 전환을 위해 만듦
     public static LessonWordQuiz3 newInstance() {
@@ -44,12 +61,14 @@ public class LessonWordQuiz3 extends Fragment implements Button.OnClickListener{
 
         view = inflater.inflate(R.layout.lesson_word_quiz3, container, false);
 
-        wordsForQuiz = LessonWord.wordFront;
+        answerLayout = view.findViewById(R.id.answerLayout);
+        tvAnswer = view.findViewById(R.id.tvAnswer);
+        flexboxLayout = view.findViewById(R.id.flexboxLayout);
+        btnAudio = view.findViewById(R.id.btnAudio);
+        btnReset = view.findViewById(R.id.btnReset);
+        btnAudio.setOnClickListener(this);
+        btnReset.setOnClickListener(this);
 
-        wordQuiz1Answer = view.findViewById(R.id.wordQuiz1Answer);
-        wordQuiz1Selector = view.findViewById(R.id.wordQuiz1Selector);
-
-        correctImage = view.findViewById(R.id.correctImage);
 
         selectorButtonClick = new View.OnClickListener() {
 
@@ -57,74 +76,68 @@ public class LessonWordQuiz3 extends Fragment implements Button.OnClickListener{
             public void onClick(View view) {
 
                 Button selectedBtn = (Button) view;
-                String selectedButton = selectedBtn.getText().toString();
-                wordQuiz1Answer.append(selectedButton);
 
-                if(wordQuiz1Answer.getText().length() == word.length()) {
+                String selectedBtnText = selectedBtn.getText().toString();
+                tvAnswer.append(selectedBtnText);
 
-                    if(wordQuiz1Answer.getText().toString().equals(word)) {
-                        // people1. 정답소리 play 추가할 것
-                        correctImage.setVisibility(View.VISIBLE);
+                btnReset.setVisibility(View.VISIBLE);
 
-                        // 1초 후에 정답이미지 GONE 하고 다음 문제 넘어감
-                        Handler handler = new Handler();
-                        handler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
+                if(tvAnswer.getText().length() == word.length()) {
+
+                    if(tvAnswer.getText().toString().equals(word)) { // 정답
+
+                        answerLayout.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_white_10_stroke_purple));
+                        mediaPlayer = MediaPlayer.create(getContext(), R.raw.correct);
+                        mediaPlayer.start();
+
+
+                    } else {  // 오답
+                        answerLayout.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_white_10_stroke_red));
+                        mediaPlayer = MediaPlayer.create(getContext(), R.raw.wrong);
+                        mediaPlayer.start();
+                    }
+
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            if(tvAnswer.getText().toString().equals(word)) {
                                 quizCount++;
 
                                 LessonFrame.progressCount++;
                                 LessonFrame.progressCount();
 
-                                if(quizCount != wordsForQuiz.length) {
-                                    correctImage.setVisibility(View.GONE);
-                                    wordQuiz1Selector.removeAllViews();
+                                if (quizCount != words.length) {  // 문제가 남아있을 때
+                                    flexboxLayout.removeAllViews();
                                     makeQuiz();
-                                } else {
+
+                                } else {  // 문제 다 풀었을 때
                                     quizCount = 0;
-                                    ((LessonFrame)getActivity()).replaceFragment(LessonSentence.newInstance());
+                                    ((LessonFrame) getActivity()).replaceFragment(LessonSentence.newInstance());
                                 }
-                                wordQuiz1Answer.setText("");
-
                             }
-                        }, 1000);
-
-                        // people1. quizNo == wordForQuiz.length 이면 Quiz2로 프레그먼트 전환
-                    } else {
-                        wordQuiz1Answer.setText("");
-                    }
+                            tvAnswer.setText("");
+                            btnReset.setVisibility(View.GONE);
+                            answerLayout.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_white_10));
+                        }
+                    }, 2000);
                 }
             }
         };
 
-
-        RandomArray.randomArray(wordsForQuiz);
+        words = LessonWord.wordFront;
 
         makeQuiz();
 
-        btnReset = view.findViewById(R.id.btnReset);
-        btnReset.setOnClickListener(this);
-
         return view;
     }
-
-    @Override
-    public void onClick(View v) {
-
-        switch (v.getId()) {
-
-            case R.id.btnReset :
-                wordQuiz1Answer.setText("");
-                break;
-        }
-    }
-
 
 
     // word의 단어를 음절로 나누고 램덤으로 섞어서 버튼으로 만들어 줌
     public void makeQuiz() {
 
-        word = wordsForQuiz[quizCount];
+        word = words[quizCount];
 
         syllables = new String[word.length()];
 
@@ -137,11 +150,35 @@ public class LessonWordQuiz3 extends Fragment implements Button.OnClickListener{
         for(int i=0; i<syllables.length; i++) {
 
             btnSelector = new Button(getContext());
-            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(DpToPx.getDpToPx(getResources(), 50), DpToPx.getDpToPx(getResources(), 50));
+
+            int gap10 = DpToPx.getDpToPx(getResources(), 10);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.rightMargin = gap10;
+            params.bottomMargin = gap10;
+
             btnSelector.setLayoutParams(params);
+            btnSelector.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_white_10));
             btnSelector.setText(syllables[i]);
             btnSelector.setOnClickListener(selectorButtonClick);
-            wordQuiz1Selector.addView(btnSelector);
+            flexboxLayout.addView(btnSelector);
         }
     }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btnReset :
+                tvAnswer.setText("");
+                btnReset.setVisibility(View.GONE);
+                break;
+
+            case R.id.btnAudio :
+                break;
+        }
+    }
+
+
 }
